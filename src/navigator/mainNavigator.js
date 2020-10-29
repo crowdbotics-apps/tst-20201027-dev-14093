af2db1da971aea6621c81b5ae0e1758812e32b14
Crowdbotics <team@crowdbotics.com>
2020-10-29 20:04:49 +0000

    remove package blueprint-react-native-blank-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import BlankScreen713522Navigator from '../features/BlankScreen713522/navigator'
 import BlankScreen813521Navigator from '../features/BlankScreen813521/navigator';
 import BlankScreen413520Navigator from '../features/BlankScreen413520/navigator';
 import BlankScreen913519Navigator from '../features/BlankScreen913519/navigator';
-import BlankScreen513516Navigator from '../features/BlankScreen513516/navigator';
 import BlankScreen413514Navigator from '../features/BlankScreen413514/navigator';
 import BlankScreen613512Navigator from '../features/BlankScreen613512/navigator';
 import BlankScreen513511Navigator from '../features/BlankScreen513511/navigator';
@@ -39,7 +38,6 @@ BlankScreen713522: { screen: BlankScreen713522Navigator },
 BlankScreen813521: { screen: BlankScreen813521Navigator },
 BlankScreen413520: { screen: BlankScreen413520Navigator },
 BlankScreen913519: { screen: BlankScreen913519Navigator },
-BlankScreen513516: { screen: BlankScreen513516Navigator },
 BlankScreen413514: { screen: BlankScreen413514Navigator },
 BlankScreen613512: { screen: BlankScreen613512Navigator },
 BlankScreen513511: { screen: BlankScreen513511Navigator },
